032e3d9f6b107f9e8d6f3f818827e202d4583223
sindyalvinaa <sindyalvinaa93@gmail.com>
2022-03-12 13:09:12 +0700

    07 - Dasar Pemrograman Laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        @forelse ($news as $item)
+        <div class="card mt-2">
+            <div class="card-header">{{$item->title}}</div>
+            <div class="card-body">{{$item->content}}</div>
+        </div>
+        @empty
+        <div class="card mt-2">
+            <div class="card-header">Belum ada data</div>
+            <div class="card-body">Belum ada Content</div>
+        </div>
+        @endforelse
+
+        </div>
+    </div>
+</div>
+@endsection
